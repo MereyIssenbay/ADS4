78a8306a7e30e610485dcad82c7b447665195c1f
Merey <misenbai@gmail.com>
2023-05-14 17:29:40 +0600

    3rd

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,20 +1,6 @@
+import java.util.LinkedList;
 public class MyHashTable<K, V> {
-    private class HashNode<K, V>{
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
-
-        public HashNode<K key, V value>{
-            this.key = key;
-            this.value = value;
-        }
-        @Override
-        public String toString(){
-            return "{" + key + " " + value + "}";
-        }
-    }
-
-    private HashNode<K,V> [] chainArray; // or Object[]
+    private LinkedList<HashNode<K, V>>[] chain;; // or Object[]
     private int M = 11; //default number of chains
     private int size;
 
